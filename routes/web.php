fb960b31a88173a26eb675dce26c58d3f6aa9833
Fauzy169 <ahmadfauzy169@gmail.com>
2024-12-04 11:24:00 +0800

    Laravel Chalance Make CRUD & Product list

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/product', function() {
+    return view('product');
+});
